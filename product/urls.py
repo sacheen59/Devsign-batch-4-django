300c98e588c24647b4d86ca4de50d42e905a53dc
Sachin Barali <baralisachin5925@gmail.com>
2026-07-05 15:41:04 +0545

    rendering template

--- product/urls.py
+++ product/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test/', views.index),
-    path('second-test/', views.second_test)
+    path('test/', views.index,name="test"),
+    path('second-test/', views.second_test),
+    path('list/', views.product_list, name="list")
 ]
--- product/views.py
+++ product/views.py
@@ -12,8 +12,80 @@ from django.http import HttpResponse
 # function based views
 
 def index(request):
-    print("when url hits this function runs")
-    return HttpResponse("This is index page")
+    # data = {
+    #     "name": "Dipesh Kharel",
+    #     "age": 21,
+    #     "course": "Python with django"
+    # }
+    student_info = [
+    {
+        "name": "Ram",
+        "marks": {
+            "math": 90,
+            "social": 20,
+            "english": 40,
+        },
+        "is_active": True
+    },
+    {
+        "name": "Hari",
+        "marks": {
+            "math": 85,
+            "social": 45,
+            "english": 35,
+        },
+        "is_active": False
+    },
+    {
+        "name": "Geeta",
+        "marks": {
+            "math": 55,
+            "social": 58,
+            "english": 29,
+        },
+        "is_active": True
+    },
+    {
+        "name": "Sita",
+        "marks": {
+            "math": 56,
+            "social": 86,
+            "english": 49,
+        },
+        "is_active": False
+    },
+]
+    return render(request, "product/index.html", {
+        "infos": student_info
+    })
 
 def second_test(request):
     return HttpResponse("This is second test page.")
+
+def product_list(request):
+    products = [
+        {
+            "id": 1,
+            "product_name": "Mobile Phone",
+            "product_price": 120000,
+            "is_available": True,
+            "image_url": "https://m.media-amazon.com/images/I/41cslOLNecL._SY300_SX300_QL70_FMwebp_.jpg"
+        },
+        {
+            "id": 2,
+            "product_name": "TV",
+            "product_price": 45000,
+            "is_available": False,
+            "image_url": "https://m.media-amazon.com/images/I/81qtWbsLU4L._SX679_.jpg"
+        },
+        {
+            "id": 3,
+            "product_name": "Laptop",
+            "product_price": 120000,
+            "is_available": True,
+            "image_url": "https://m.media-amazon.com/images/I/717WZ7WriwL._SX679_.jpg"
+        }
+    ]
+    return render(request, "product/list.html",{
+        "products": products
+    })
