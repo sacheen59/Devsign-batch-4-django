f9aee255df2667d525d5fccd9fe6e2c8b9d7ac1a
Sachin Barali <baralisachin5925@gmail.com>
2026-07-22 15:53:55 +0545

    crispy forms

--- product/forms.py
+++ product/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import Category
 
 # there are two type of forms in django
@@ -11,6 +12,11 @@ from .models import Category
 
 # model form
 
+# form validation in django
+# field level validation => validating each individual field
+# clean_<field_name>(self)
+# object level validation => validating between two or more fields
+
 class CategoryForm(forms.ModelForm):
     # price = forms.IntegerField()
 
@@ -20,5 +26,15 @@ class CategoryForm(forms.ModelForm):
         # fields = "__all__"
         # exclude = ["description"]
 
+    # field level validation
+    def clean_category_name(self):
+        # extract the category_name
+        category_name = self.cleaned_data.get('category_name')
+        # check whether the category name already exists in db or not
+        if len(category_name) < 3:
+            raise forms.ValidationError("Category name must be more than 3 characters")
+        # if yes show validation
+        # else return the category_name
+        return category_name
 
 # {'category_name' : value}
