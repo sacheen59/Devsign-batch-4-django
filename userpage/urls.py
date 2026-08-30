3f3ad8fd042594f7701eaf5cfc14f807ba8a24dd
Sachin Barali <baralisachin5925@gmail.com>
2026-08-30 15:18:14 +0545

    product and product detail

--- userpage/urls.py
+++ userpage/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.homepage, name="homepage")
+    path('',views.homepage, name="homepage"),
+    path('all-products/',views.all_products, name="productpage"),
+    path('product/<int:product_id>/', views.product_detail, name="product-detail")
 ]
--- userpage/views.py
+++ userpage/views.py
@@ -8,3 +8,15 @@ def homepage(request):
     return render(request, "userpage/homepage.html", {
         "products": products
     })
+
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, "userpage/products.html", {
+        "products": products
+    })
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    return render(request, "userpage/product-detail.html", {
+        "product": product
+    })
